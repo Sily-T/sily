69af8945dc81e32e0866b06566005577f81167c5
Fakam-Choy <fakam_choy@foxmail.com>
2018-01-03 17:33:57 +0800

    add sysDept

---
+++ sily-sys-service/src/main/java/com/sily/service/Impl/SysDeptServiceImpl.java
@@ -0,0 +1,61 @@
+package com.sily.service.Impl;
+import java.util.List;
+import com.sily.dao.SysDeptDao;
+import com.sily.api.SysDept;
+import com.sily.service.SysDeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class SysDeptServiceImpl implements SysDeptService{
+    @Autowired
+    private SysDeptDao sysDeptDao;
+    @Override
+    public long getSysDeptRowCount(){
+        return sysDeptDao.getSysDeptRowCount();
+    }
+    @Override
+    public List<SysDept> selectSysDept(){
+        return sysDeptDao.selectSysDept();
+    }
+    @Override
+    public SysDept selectSysDeptByObj(SysDept obj){
+        return sysDeptDao.selectSysDeptByObj(obj);
+    }
+    @Override
+    public SysDept selectSysDeptById(Long id){
+        return sysDeptDao.selectSysDeptById(id);
+    }
+    @Override
+    public int insertSysDept(SysDept value){
+        return sysDeptDao.insertSysDept(value);
+    }
+    @Override
+    public int insertNonEmptySysDept(SysDept value){
+        return sysDeptDao.insertNonEmptySysDept(value);
+    }
+    @Override
+    public int insertSysDeptByBatch(List<SysDept> value){
+        return sysDeptDao.insertSysDeptByBatch(value);
+    }
+    @Override
+    public int deleteSysDeptById(Long id){
+        return sysDeptDao.deleteSysDeptById(id);
+    }
+    @Override
+    public int updateSysDeptById(SysDept enti){
+        return sysDeptDao.updateSysDeptById(enti);
+    }
+    @Override
+    public int updateNonEmptySysDeptById(SysDept enti){
+        return sysDeptDao.updateNonEmptySysDeptById(enti);
+    }
+
+    public SysDeptDao getSysDeptDao() {
+        return this.sysDeptDao;
+    }
+
+    public void setSysDeptDao(SysDeptDao sysDeptDao) {
+        this.sysDeptDao = sysDeptDao;
+    }
+
+}
