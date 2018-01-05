f1497bf7e6bc5224d79627c97e5da43e279c68a9
admin <jdzgd@qq.com>
2018-01-05 17:28:29 +0800

    1

--- sily-sys-web/src/main/webapp/static/js/alluser.js
+++ sily-sys-web/src/main/webapp/static/js/alluser.js
@@ -10,8 +10,30 @@ $().ready(function () {
         url: "/user/alluser",
         async: true,
         success: function (result) {
-            for (var i = 0; i < result.length; i++) {
-                $tableFirstTr.append("<tr><td>"+result[i].id+"</td><td>"+result[i].account+"</td><td>"+result[i].userType+"</td><td>"+result[i].userName+"</td><td>"+result[i].sex+"</td><td>"+result[i].phone+"</td><td>"+result[i].email+"</td><td>是否启用</td></tr>");
+            var userEnable = "启用";
+            for (var i = 0; i++; i < result.length) {
+                //因为多次使用userid，先建变量
+                var userID = result[i].id;
+                //将userEnable的值转变为文字
+                if (result[i].enable == 0) {
+                    userEnable = "禁用";
+                }
+                userEnable = "启用";
+                $tableFirstTr.append("<tr></tr><td>" + result[i].id + "</td><td>" + result[i].account + "</td><td>" + result[i].userType + "</td><td>" + result[i].userName + "</td><td>" + result[i].sex + "</td><td>" + result[i].phone + "</td><td>" + result[i].email + "</td><td>result[i].enable</td> <td>\n" +
+                    "                        <a href='singleuser.html?" + userID + "'>\n" +
+                    "                            <button class='btn btn-info'>查看</button>\n" +
+                    "                        </a>\n" +
+                    "                    </td>\n" +
+                    "                    <td>\n" +
+                    "                        <a href='singleuser.html?" + userID + "'>\n" +
+                    "                            <button class='btn btn-warning'>编辑</button>\n" +
+                    "                        </a>\n" +
+                    "                    </td>\n" +
+                    "                    <td>\n" +
+                    "                        <a href='singleuser.html?" + userID + "'>\n" +
+                    "                            <button class='btn btn-danger'>删除</button>\n" +
+                    "                        </a>\n" +
+                    "                    </td></tr>");
             }
         },
         error: function (result) {
--- sily-sys-web/src/main/webapp/static/js/singleuser.js
+++ sily-sys-web/src/main/webapp/static/js/singleuser.js
@@ -1,3 +1,5 @@
 $().ready(function () {
+    // 获取当前用户id，url问号后面的值
+    var userID = location.search.substring(1);
 
 })
