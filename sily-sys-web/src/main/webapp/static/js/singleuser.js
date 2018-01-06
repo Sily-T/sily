7f93c8932d6a47b80cf012ad4a09cc9c652ca421
admin <jdzgd@qq.com>
2018-01-06 15:12:58 +0800

    role

--- sily-sys-web/src/main/webapp/static/js/singleuser.js
+++ sily-sys-web/src/main/webapp/static/js/singleuser.js
@@ -3,6 +3,59 @@ $().ready(function () {
     // 获取当前用户id，url问号后面的值
     var userID = location.search.substring(1);
     var jsonUserId = {"id": userID};
+    //请求所有角色信息,此处还未修改
+    // ajaxAllRole();
+    //请求该用户的信息
+    ajaxUserInfo(jsonUserId);
+    //保存按钮：修改该用户信息
+    $('#save').click(function () {
+        //获取该用户修改后的值，并组装承json对象
+        var jsonUpdateUser=returnUpdateUserInfoJson();
+        //向后端请求修改用户的信息
+        $.ajax({
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            url: "/user/updateSingleuser",
+            async: true,
+            data: JSON.stringify(jsonUpdateUser),
+            success: function (result) {
+                console.log("success222");
+            },
+            error: function (result) {
+                console.log("fail");
+                // alert("XMLHttpRequest.status=" + XMLHttpRequest.status + "\n textStatus=" + textStatus + "\n errorThrown=" + errorThrown);
+            }
+        })
+    })
+});
+
+//请求所有角色信息
+function ajaxAllRole() {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        url: "",
+        async: false,
+        success: function (result) {
+            //生成所有角色dom节点
+            for (var i = 0; i < result.length; i++) {
+                $('#ol-roles').append("<li>\n" +
+                    "                                <div class='checkbox'>\n" +
+                    "                                    <label>\n" +
+                    "                                        <input type='checkbox'> +result[i]+\n" +
+                    "                                    </label>\n" +
+                    "                                </div>\n" +
+                    "                            </li>");
+            }
+        },
+        error: function (result) {
+            console.log("ajaxAllRole fail");
+        }
+    })
+}
+
+//请求该用户的信息
+function ajaxUserInfo(jsonUserId) {
     $.ajax({
         type: "POST",
         contentType: "application/json;charset=utf-8",
@@ -24,40 +77,26 @@ $().ready(function () {
         },
         error: function (result) {
             console.log("fail");
-            alert("XMLHttpRequest.status=" + XMLHttpRequest.status + "\n textStatus=" + textStatus + "\n errorThrown=" + errorThrown);
         }
-    })
-
-    $('#save').click(function () {
-        if($("input[type='radio']:checked").val() == null){
-            var sex = 0;
-        }
-        var jsonUser = {
-            "id": userID,
-            "userName": $('#user-name').val(),
-            "namePinyin":$('#name-pinyin').val(),
-            "phone": $('#phone').val(),
-            "email": $('#email').val(),
-            "idCard": $('#id-card').val(),
-            "address": $('#address').val(),
-            "sex": sex,
-            "birthday":$('#birthday').val(),
-            "deptId":$('#dept-id').val(),
-            "enable":$("#select option:selected").val()
-        };
-        $.ajax({
-            type: "POST",
-            contentType: "application/json;charset=utf-8",
-            url: "/user/updateSingleuser",
-            async: true,
-            data: JSON.stringify(jsonUser),
-            success: function (result) {
-                console.log("success222");
-            },
-            error: function (result) {
-                console.log("fail");
-                // alert("XMLHttpRequest.status=" + XMLHttpRequest.status + "\n textStatus=" + textStatus + "\n errorThrown=" + errorThrown);
-            }
-        })
-    })
-});
+    });
+}
+//获取该用户修改后的值，并组装承json对象
+function returnUpdateUserInfoJson () {
+    if ($("input[type='radio']:checked").val() == null) {
+        var sex = 0;
+    }
+    var jsonUser = {
+        "id": userID,
+        "userName": $('#user-name').val(),
+        "namePinyin": $('#name-pinyin').val(),
+        "phone": $('#phone').val(),
+        "email": $('#email').val(),
+        "idCard": $('#id-card').val(),
+        "address": $('#address').val(),
+        "sex": sex,
+        "birthday": $('#birthday').val(),
+        "deptId": $('#dept-id').val(),
+        "enable": $("#select option:selected").val()
+    };
+    return jsonUser
+}
