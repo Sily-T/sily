947f1702affa4c33845c0bcf12cc132d2a55f54f
admin <jdzgd@qq.com>
2018-01-15 14:38:06 +0800

    fix bug :show sex

--- sily-sys-web/src/main/webapp/static/js/singleuser.js
+++ sily-sys-web/src/main/webapp/static/js/singleuser.js
@@ -1,5 +1,6 @@
 $().ready(function () {
-    $("input[type='radio']:checked").val(1);
+    //男性别选取示例
+    // $("input[name='sex']:first").attr("checked",true);
     // 获取当前用户id，url问号后面的值
     var userID = location.search.substring(1);
     var jsonUserId = {"id": userID};
@@ -40,13 +41,6 @@ function ajaxAllRole() {
         success: function (allRole) {
             //生成所有角色dom节点
             for (var i = 0; i < allRole.length; i++) {
-                // $('#ol-roles').append("<li>\n" +
-                //     "                                <div class='checkbox'>\n" +
-                //     "                                    <label>\n" +
-                //     "                                        <input id='"+allRole[i].id+"' type='checkbox'> "+allRole[i].roleName+"\n" +
-                //     "                                    </label>\n" +
-                //     "                                </div>\n" +
-                //     "                            </li>");
 
                 $('#ol-roles').append("         <li>\n" +
                     "                                <label class='checkbox-inline'>\n" +
@@ -70,14 +64,24 @@ function ajaxUserInfo(jsonUserId) {
         async: true,
         data: JSON.stringify(jsonUserId),
         success: function (result) {
-            console.log("success");
+            // 2018年1月15日，
+            if (result.sex==1){
+                $("input[name='sex']").eq(0).attr("checked",true);
+                console.log("显示男性");
+            }else if (result.sex==2){
+                $("input[name='sex']").eq(1).attr("checked",true);
+                console.log("显示女性");
+            }else {
+                console.log("显示未知性别");
+            }
+
             $('#user-name').val(result.userName);
             $('#name-pinyin').val(result.namePinyin);
             $('#phone').val(result.phone);
             $('#email').val(result.email);
             $('#id-card').val(result.idCard);
             $('#address').val(result.address);
-            $("input[type='radio']:checked").val(result.sex);
+            // $("input[type='radio']:checked").val(result.sex);
             $('#birthday').val(result.birthday);
             $('#dept-id').val(result.deptId);
             $("#select option:selected").val(result.enable);
@@ -89,10 +93,23 @@ function ajaxUserInfo(jsonUserId) {
 }
 //获取该用户修改后的值，并组装承json对象
 function returnUpdateUserInfoJson (userID) {
-    var sex=$("input[name='sex']:checked").val();
-    if ( sex== null) {
-         sex = 0;
+    //2018年1月15日修复,为sex赋值
+    // 性别(0:未知;1:男;2:女)
+    var sex;
+    if ($("input[name='sex']").eq(0).is(':checked')){
+        sex = 1;
+        console.log("选中男性");
+    }else if ($("input[name='sex']").eq(1).is(':checked')){
+        console.log("选中女性");
+        sex = 2;
+    }else {
+        sex = 0;
+        console.log("未选中性别");
     }
+    // var sex=$("input[name='sex']:checked").val();
+    // if ( sex== null) {
+    //      sex = 0;
+    // }
     var jsonUser = {
         "id": userID,
         "userName": $('#user-name').val(),
